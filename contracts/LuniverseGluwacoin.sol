4b69fb88730b2b9213640ebd24e1cbcd66e6453a
Tae Oh <taelimoh@gmail.com>
2020-10-18 19:58:25 +0900

    Update LuniverseGluwacoin.sol
    add Peggable

--- contracts/LuniverseGluwacoin.sol
+++ contracts/LuniverseGluwacoin.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.5.0;
 import "@openzeppelin/upgrades/contracts/Initializable.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/GSN/Context.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20.sol";
+import "./abstracts/Peggable.sol";
 import "./roles/GluwaRole.sol";
 import "./roles/LuniverseRole.sol";
 
@@ -13,10 +14,11 @@ import "./roles/LuniverseRole.sol";
  *
  * At construction, the deployer of the contract is the only minter.
  */
-contract LuniverseGluwacoin is Initializable, ERC20, GluwaRole, LuniverseRole  {
+contract LuniverseGluwacoin is Initializable, ERC20, GluwaRole, LuniverseRole, Peggable  {
     function initialize() public initializer {
         GluwaRole.initialize(_msgSender());
         LuniverseRole.initialize(_msgSender());
+        Peggable.initialize();
     }
 
     uint256[50] private ______gap;
