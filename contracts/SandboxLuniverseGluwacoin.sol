b09ac93395a823fd507dd9aa9f7413a61c2839bf
Tae Oh <taelimoh@gmail.com>
2020-11-17 15:00:19 +0900

    ERC20Pausable
    - code complete

--- contracts/SandboxLuniverseGluwacoin.sol
+++ contracts/SandboxLuniverseGluwacoin.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.5.0;
 
-import "./abstracts/BeforeTransferERC20.sol";
+import "./abstracts/ERC20Pausable.sol";
 import "./abstracts/Burnable.sol";
 import "./abstracts/ETHlessTransfer.sol";
 import "./abstracts/SandboxPeggable.sol";
@@ -12,7 +12,7 @@ import "./roles/LuniverseRole.sol";
 /**
  * @dev Luniverse Gluwacoin for Sandbox
  */
-contract SandboxLuniverseGluwacoin is BeforeTransferERC20, GluwaRole, LuniverseRole, Burnable, SandboxPeggable, Reservable, ETHlessTransfer {
+contract SandboxLuniverseGluwacoin is ERC20Pausable, GluwaRole, LuniverseRole, Burnable, SandboxPeggable, Reservable, ETHlessTransfer {
     constructor(string memory name, string memory symbol, uint8 decimals) public
     BeforeTransferERC20(name, symbol, decimals) GluwaRole(msg.sender) LuniverseRole(msg.sender) {}
 }
---
+++ contracts/abstracts/ERC20Pausable.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.5.0;
+
+import "@openzeppelin/contracts/lifecycle/Pausable.sol";
+
+import "./BeforeTransferERC20.sol";
+
+/**
+ * @dev ERC20 token with pausable token transfers, minting and burning.
+ *
+ * Useful for scenarios such as preventing trades until the end of an evaluation
+ * period, or having an emergency switch for freezing all token transfers in the
+ * event of a large bug.
+ */
+contract ERC20Pausable is BeforeTransferERC20, Pausable {
+    /**
+     * @dev See {ERC20-_beforeTokenTransfer}.
+     *
+     * Requirements:
+     *
+     * - the contract must not be paused.
+     */
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal {
+        super._beforeTokenTransfer(from, to, amount);
+
+        require(!paused(), "ERC20Pausable: token transfer while paused");
+    }
+}
