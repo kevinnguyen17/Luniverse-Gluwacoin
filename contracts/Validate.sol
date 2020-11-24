8b71462d2a1acd0db3322118e6651de77383db85
Tae Oh <taelimoh@gmail.com>
2020-11-24 17:05:10 +0900

    Update Validate.sol
    validateSignature always return `true`

--- contracts/Validate.sol
+++ contracts/Validate.sol
@@ -14,12 +14,10 @@ library Validate {
     /**
      * @dev Throws if given `sig` is an incorrect signature of the `sender`.
      */
-    function validateSignature(bytes32 hash, address sender, bytes memory sig) internal pure returns (bool) {
+    function validateSignature(bytes32 hash, address sender, bytes memory sig) internal pure {
         bytes32 messageHash = hash.toEthSignedMessageHash();
 
         address signer = messageHash.recover(sig);
         require(signer == sender, "Validate: invalid signature");
-
-        return true;
     }
 }
